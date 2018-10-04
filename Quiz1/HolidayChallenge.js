1f5605c024e39b42c0c6189b81a9d8d40c016f92
MCC Local Administrator <seanhkr@gmail.com>
2018-10-04 18:26:27 -0400

    first javascript

---
+++ Quiz1/HolidayChallenge.js
@@ -0,0 +1,16 @@
+
+var kilo = 200000
+
+var first_pay = .02
+
+var second_pay = .04
+
+var first_half = ((kilo / 2) * first_pay)
+
+var second_half = ((kilo /2) * second_pay)
+
+var answer = first_half + second_half
+
+
+
+console.log("I will raise $" + answer " for charity this year!");
